943b3b51b59f6ac933d4c86f2557ee5f4ee37721
Sitaram <atistechnlogy@gmail.com>
2023-04-16 13:17:06 +0530

    .gitignore file is added and all .class files ignored

---
+++ AmericanFlag.java
@@ -0,0 +1,30 @@
+/*
+ * Java Basic Examples Pracitce 
+ * @version 1.0.0
+ * @author Sitaram
+ * Source of Exercises: https://www.w3resource.com/java-exercises/basic/index.php
+ * Example 14: Drawing American flag 16/04/2023
+ */
+public class AmericanFlag {
+    public static void main(String[] args){ 
+        System.out.println("* * * * * * * * =============================");
+        System.out.println(" * * * * * * * * ============================");
+        System.out.println("* * * * * * * * =============================");
+        System.out.println(" * * * * * * * * ============================");
+        System.out.println("* * * * * * * * =============================");
+        System.out.println(" * * * * * * * * ============================");
+        System.out.println("* * * * * * * * =============================");
+        System.out.println(" * * * * * * * * ============================");
+        System.out.println("* * * * * * * * =============================");
+        System.out.println("=============================================");
+        System.out.println("=============================================");
+        System.out.println("=============================================");
+        System.out.println("=============================================");
+        System.out.println("=============================================");
+        System.out.println("=============================================");
+        System.out.println("=============================================");
+        System.out.println("=============================================");
+        
+    }
+}
+
