6c98ac7b5ca3b6586ab9bdd93aa66701dce3ea19
Sitaram <atistechnlogy@gmail.com>
2023-04-18 11:07:54 +0530

    Array Intialisation Example

---
+++ Arrays/InitialiseArray.java
@@ -0,0 +1,15 @@
+/*
+ * Project: Arrays
+ * version: 1.0.0
+ * @author: sitaram Date: 18/04/2023
+ * Idea: To understand how Arrays works.
+ * Problem Description: Initialise the array and print the Elements in it.
+ */
+public class InitialiseArray {
+   public static void main(String[] args){
+    int Array[] = {1,2,3,4,5};
+    for(int i =0; i<Array.length; i++){
+        System.out.print(" " + Array[i] + "\n");
+    }
+   } 
+}
