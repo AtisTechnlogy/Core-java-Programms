9afff6ebe96a16820af14f8e8ab36d60cbe84ad3
Sitaram <atistechnlogy@gmail.com>
2023-05-22 10:14:26 +0530

    leve2 from exercise 19

---
+++ 150Exercises/Exercise19.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Exercise19 
+{
+    public static void main(String[] args)
+    {
+        int[] bin_num = new int[16];
+        int  i=0, quot;
+        Scanner UserInput = new Scanner(System.in);
+        System.out.print("Enter a Decimal Number: ");
+        int dec_num = UserInput.nextInt();
+        quot = dec_num;
+
+        while(quot !=0)
+        {
+            bin_num[i++] = quot %2;
+            quot = quot /2;
+        }
+        System.out.println(i);
+        System.out.println(dec_num + " in binary format: " );
+        while(i>=0)
+        {
+            System.out.print(bin_num[i--]);
+        }
+        System.out.println();
+    }
+}
